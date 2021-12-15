d975a60606257399c268fc47034218235e307e54
Thomas Fournier <thomas@artivain.com>
2021-12-14 22:10:23 -0500

    Retrait d'un message non nécessaire maintenant

--- ab-core/events/onReadyMessage.js
+++ ab-core/events/onReadyMessage.js
@@ -3,7 +3,6 @@ module.exports = {
 	once: true,
 
 	execute(client) {
-		console.log("Started"); // Pour les serveurs pterodactyl utilisant l'egg de Artivain, comme les hébergements Gameverse
 		console.log(`[Artibot] Prêt! Connecté en tant que ${client.user.tag}`);
 	},
 };
