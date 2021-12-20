01631ebdf6771c8440397a8f1cc313ce85bcb286
Thomas Fournier <thomas@artivain.com>
2021-12-20 00:55:38 -0500

    Updater presque terminé Le testing et le peaufinage, demain. Pour l'instant, dodo.

--- ab-core/updater.js
+++ ab-core/updater.js
@@ -1,19 +1,83 @@
 const AutoGitUpdate = require('auto-git-update');
+const { params } = require("../config.json");
 
-const config = {
-	repository: 'https://github.com/Artivain/artibot',
-	tempLocation: '../updaterbk',
-	branch: "main",
-	exitOnComplete: true
+// if ran with npm run updater
+if (require.main === module) {
+
+	if (params.checkForUpdates != "stable" && params.checkForUpdates != "unstable") {
+		console.error("[Updater] Erreur de configuration");
+		process.exit(1);
+	};
+
+	const config = {
+		repository: 'https://github.com/Artivain/artibot',
+		tempLocation: './updaterFiles',
+		exitOnComplete: true,
+		branch: (params.checkForUpdates == "stable" ? "main" : "unstable")
+	};
+
+	const updater = new AutoGitUpdate(config);
+
+	updater.setLogConfig({
+		logDebug: true,
+		logDetail: true,
+		logGeneral: true,
+		logWarning: true,
+		logError: true
+	});
+	updater.autoUpdate();
+
+} else {
+
+	module.exports = {
+		async checkUpdates() {
+			if (params.checkForUpdates != "stable" && params.checkForUpdates != "unstable") {
+				return console.log("[Updater] Vérification des mises à jours désactivée dans la configuration")
+			}
+
+			const config = {
+				repository: 'https://github.com/Artivain/artibot',
+				tempLocation: './updaterFiles',
+				exitOnComplete: true,
+				branch: (params.checkForUpdates == "stable" ? "main" : "unstable")
+			};
+
+			const updater = new AutoGitUpdate(config);
+
+			updater.setLogConfig({
+				logDebug: false,
+				logDetail: false,
+				logGeneral: false,
+				logWarning: true,
+				logError: true
+			});
+
+			return updater.compareVersions();
+		},
+
+		async doUpdates() {
+			if (params.checkForUpdates != "stable" && params.checkForUpdates != "unstable") {
+				throw "Erreur de configuration"
+			};
+		
+			const config = {
+				repository: 'https://github.com/Artivain/artibot',
+				tempLocation: './updaterFiles',
+				exitOnComplete: false,
+				branch: (params.checkForUpdates == "stable" ? "main" : "unstable")
+			};
+		
+			const updater = new AutoGitUpdate(config);
+		
+			updater.setLogConfig({
+				logDebug: true,
+				logDetail: true,
+				logGeneral: true,
+				logWarning: true,
+				logError: true
+			});
+			return updater.autoUpdate();
+		}
+	};
+
 };
-
-const updater = new AutoGitUpdate(config);
-
-updater.setLogConfig({
-	logDebug: true,
-	logDetail: true,
-	logGeneral: true,
-	logWarning: true,
-	logError: true
-});
-updater.autoUpdate();
---
+++ ab-modules/commands/core/update.js
@@ -0,0 +1,43 @@
+const updater = require("../../../ab-core/updater");
+
+module.exports = {
+	name: "update",
+	description: "Installe les mises à jour du bot",
+	ownerOnly: true,
+
+	execute(message, args, config) {
+		// Check if config is valid
+		if (config.checkForUpdates != "stable" && config.checkForUpdates != "unstable") {
+			message.reply("Le canal des mises à jour n'est pas défini dans la config!");
+			return
+		}
+
+		// If the owner really wants to do the update, let's do it
+		if (args[0] == "force") {
+			updater.checkUpdates().then(response => {
+				if (response.upToDate) {
+					message.reply(`Artibot est déjà à jours (v${response.currentVersion})`)
+					return
+				};
+
+				updater.doUpdates()
+					.then(response => {
+						if (!response) {
+							message.reply("Une erreur est survenue pendant la mise à jour. Consulter la console pour plus de détails.");
+							return
+						}
+						message.reply(
+							"La mise à jour a bien été installée.\n" +
+							"Le bot va maintenant s'éteindre tout seul.\n" + 
+							"Si votre hébergement le supporte, le bot devrait revenir de lui même bientôt."
+						);
+						process.exit(1);
+					})
+					.catch(e => {
+						message.reply("Une erreur est survenue pendant la mise à jour. Consulter la console pour plus de détails.");
+						console.error("[Updater] Problème avec la mise à jour: " + e);
+					});
+			});
+		};
+	},
+};
