65fc2e39daa90de502ef7a3514a8fd49cb46f593
Artivain <thomas@artivain.tk>
2022-02-12 23:29:47 -0500

    Migrated reactionrole module

---
+++ ab-modules/reactionRole/index.js
@@ -0,0 +1,33 @@
+/**
+ * Reaction Roles module for Artibot
+ * Based on Node.js module discordjs-reaction-role
+ * @author GoudronViande24
+ */
+
+module.exports = {
+	name: "ReactionRoles",
+
+	manifest: {
+		manifestVersion: 1,
+		moduleVersion: "1.1.0",
+		name: "ReactionRoles",
+		supportedLocales: [
+			"en",
+			"fr"
+		],
+		parts: [
+			{
+				id: "reactionrole",
+				type: "global",
+				path: "index.js"
+			}
+		]
+	},
+
+	async execute({ client }) {
+		const ReactionRole = require("discordjs-reaction-role").default;
+		const config = require("./config.json");
+
+		new ReactionRole(client, config);
+	}
+};
