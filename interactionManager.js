3e9aa87288dba27c6720fca061bc77dd1b520d2f
Thomas Fournier <thomas@artivain.com>
2022-04-25 18:09:42 -0400

    Cleanup unused files Update dependencies Update Node.js Prepare jsdoc

--- interactionManager.js
+++ interactionManager.js
@@ -1,6 +1,5 @@
 import { REST } from "@discordjs/rest";
 import { Routes } from "discord-api-types/v9";
-import { Client } from "discord.js";
 import Artibot, { log, Module } from "./index.js";
 
 /**
