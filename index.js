76d9b7aa01b06b8d391aa0ee4869439d66ed536f
Thomas Fournier <thomas@artivain.com>
2022-04-27 23:26:17 -0400

    try to fix localizer problem

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ export class Artibot {
 
 		// Create a localizer for the core
 		this.localizer = new Localizer({
-			filePath: "locales.json",
+			filePath: path.join(__dirname, "locales.json"),
 			lang
 		});
 
