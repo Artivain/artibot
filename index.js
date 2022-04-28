445d2115a56176f8d9c7b16a0ac1a70dd02a657b
Thomas Fournier <thomas@artivain.com>
2022-04-27 21:01:41 -0400

    I really can't read

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const __dirname = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const require = createRequire(import.meta.url);
 const { version } = require('./package.json');
